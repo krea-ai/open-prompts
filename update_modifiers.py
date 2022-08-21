17a4ca75fdab10265f579f75c0a80b73defec34a
vipermu <vipermu97@gmail.com>
2022-08-21 15:13:44 +0200

    fix: update modifiers id

--- update_modifiers.py
+++ update_modifiers.py
@@ -7,7 +7,7 @@ for modifier_category_idx, modifier_category_path in enumerate(
         glob.glob('modifiers/*')):
     modifier_category_name = modifier_category_path.split('/')[1]
     modifier_category_data = {
-        'idx': modifier_category_idx,
+        'id': modifier_category_idx + 1,
         'name': modifier_category_name,
         'subcategories': [],
     }
@@ -17,7 +17,7 @@ for modifier_category_idx, modifier_category_path in enumerate(
         modifier_subcategory = subcategory_modifier_path.split('/')[2].split(
             '.')[0]
         modifier_subcategory_data = {
-            'id': subcategory_modifier_idx,
+            'id': subcategory_modifier_idx + 1,
             'name': modifier_subcategory,
             'modifiers': []
         }
@@ -28,7 +28,7 @@ for modifier_category_idx, modifier_category_path in enumerate(
             for subcategory_idx, subcategory_modifier in enumerate(
                     subcategory_modifiers):
                 subcategory_modifier_data = {
-                    'id': subcategory_idx,
+                    'id': subcategory_idx + 1,
                     'name': subcategory_modifier
                 }
                 modifier_subcategory_data['modifiers'].append(
